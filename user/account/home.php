2fc2ed6b6e8ece87db82a5a73d1908e14c47584f
shiva <shiva@shiva-laptop>
2012-01-11 07:04:00 +0530

    -Home page design started

--- user/account/home.php
+++ user/account/home.php
@@ -15,42 +15,78 @@ $user_data=get_user_data($user_id);
 
 <link media="screen" type="text/css" href="/styles/style.css"
 	rel="stylesheet" />
-
+<link media="screen" type="text/css" href="/styles/home.css"
+	rel="stylesheet" />
+<link media="screen" type="text/css" href="/styles/menu.css"
+	rel="stylesheet" />
 </head>
 <body>
-	<div class="home-page">
-		<div class="leftside">
-		<div class="profile-image">
-		<img alt="Profile" src="/user/account/uploaded_files/shiva.jpg" width="100px" height="100px">
-		</div>
-			<div class="hovermenu">
-				<ul>
-					<li><a href="<?php $config->getUserHomePage();?>">News feed</a></li>
-				</ul>
-				<ul>
-					<li><a href="<?php $config->getUserHomePage();?>">College</a>
-					</li>
-				</ul>
-				<ul>
-					<li><a href="<?php $config->getUserHomePage();?>">Upcoming Events</a></li>
-				</ul>
-				<ul>
-					<li><a href="<?php $config->getUserHomePage();?>">Topics List</a></li>
-				</ul>
-				<ul>
-					<li><a href="<?php $config->getUserHomePage();?>">xyz</a></li>
-				</ul>
+	<div class="globalContainer">
+		<div class="content">
+			<div class="mainContainer">
+				<div class="leftColContainer">
+					<div class="leftCol">
+						<div class="profile-picture">
+							<img alt="Profile" src="/user/account/uploaded_files/shiva.jpg"
+								width="100px" height="100px">
+						</div>
+
+						<div class="hovermenu">
+							<ul>
+								<li><a href="<?php $config->getUserHomePage();?>">News feed</a>
+								</li>
+							</ul>
+							<ul>
+								<li><a href="<?php $config->getUserHomePage();?>">College</a>
+								</li>
+							</ul>
+							<ul>
+								<li><a href="<?php $config->getUserHomePage();?>">Upcoming
+										Events</a></li>
+							</ul>
+							<ul>
+								<li><a href="<?php $config->getUserHomePage();?>">Topics List</a>
+								</li>
+							</ul>
+							<ul>
+								<li><a href="<?php $config->getUserHomePage();?>">xyz</a></li>
+							</ul>
+						</div>
+						<div class="friends">
+							<table>
+								<tr>
+									<td><?php get_my_friends($user_id) ?>
+									</td>
+								</tr>
+							</table>
+						</div>
+					</div>
+				</div>
+				<div class="contentCol">
+					<div class="rightCol">
+						<h3>Right content goes here</h3>
+					</div>
+					<div class="contentArea">
+						<table>
+							<tr>
+								<td width="20%">
+									<table>
+										<tr>
+											<th><h2>My Threads</h2></th>
+										</tr>
+										<?php
+										get_my_topics($user_id);
+										?>
+									</table>
+								</td>
+
+							</tr>
+						</table>
+					</div>
+					
+				</div>
 			</div>
-			<div class="friends">
-			<table>
-				<tr>
-					<td><?php get_my_friends($user_id) ?>
-					</td>
-				</tr>
-			</table>
 		</div>
-		</div>
-		
 		<!--  
 		<table>
 			<tr>
@@ -61,23 +97,9 @@ $user_data=get_user_data($user_id);
 			</tr>
 		</table>
 		
-		<table>
-			<tr>
-				<td width="20%">
-					<table>
-						<tr>
-							<th><h2>My Threads</h2></th>
-						</tr>
-						<?php
-						get_my_topics($user_id);
-						?>
-					</table>
-				</td>		
-				
-			</tr>
-		</table>
-	-->
 		
+	-->
+
 	</div>
 </body>
 </html>
